ce2cec596d5a9eafb97ae66aaaeca7cb88b696e5
ellie <ellieyhc@gmail.com>
2020-12-31 18:33:06 -0800

    Structure for gunicorn

--- src/gacha/gacha.py
+++ src/gacha/gacha.py
@@ -2,22 +2,24 @@ from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-app = Flask(__name__, static_folder=os.environ['STATIC_PATH'])
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['URI']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from schema import *
 
-@app.route('/')
 @app.route('/rewards')
 def gatcha():
     return render_template('rewards.html')
 
-@app.route('/redeem', methods=['POST'])
+@app.route('/rewards/redeem', methods=['POST'])
 def redeem():
     reward_type = request.form.get("type")
     if reward_type is None:
         return "Error"
 
     return "hey"
+
+if __name__ == '__main__':
+    app.run()
