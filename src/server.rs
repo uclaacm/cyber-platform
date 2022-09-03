a32a1e046b9a68b49b68beed3b0ed45d702ce488
Benson Liu <bensonhliu@gmail.com>
2022-09-03 14:38:33 -0700

    refactor styling

--- src/server.rs
+++ src/server.rs
@@ -55,10 +55,10 @@ fn make_body(page: &str, content: Markup, mut client: Client, session: String) -
 						// span { img src="/static/wordmark.svg"; }
 					}
 					ul {
-						li { a href="/pbr" {"PBR"} }
 						li { a href="/events" { "Events" } }
 						li { a href="/challenges" { "Challenges" } }
 						li { a href="/scoreboard" { "Scoreboard" } }
+						li { a href="/pbr" {"PBR"} }
 						li { a href="/internship" { "Internship" } }
 						@if count > 0 {
 							// li { a href="/rewards" { "Rewards" } }
@@ -320,13 +320,10 @@ fn get_pbr(client: Client, session: String) -> Result<impl Reply, Rejection> {
 		h1 { "Psi Beta Rho" }
 		h2 { "What is Psi Beta Rho?"}
 		p { "Psi Beta Rho, also known as PBR, is UCLA's competetive cybersecurity team! We are a group of students who are passionate about learning more about cybersecurity and competing at CTFs. PBR attends a variety of different cybersecurity competitions throughout the year by we primarily compete in capture the flag (CTF) competitions." }
-		br{}
 		h2 { "What are CTFs?"}
 		p {"CTFs or capture the flag competitions are jeapardy style cybersecurity competitions where teams compete to solve a variety of problems from various different categories. The goal of each challenge is to find a particular flag which is some sort of string which can be submitted to earn points. For an example of what CTF challenges look like, be sure to check out the Challenges page on this site."}
-		br{}
 		h2 { "Do I need to have a ton of cybersecurity experience?"}
 		p { "No! Psi Beta Rho is open to all skill levels. Whether you are just launching your cybersecurity career or are already an experienced professional, PBR is for you!" }
-		br{}
 		h2 { "How can I join?" }
 		p { "If you are on the ACM Cyber mailing list, we will make an announcement when applications to join PBR are open. We also run weekly practices that are open to any UCLA student so feel free to stop by to check out the team! For more information or to ask questions, be sure to join the ACM Cyber Discord." }
 	}, client, session)?)
